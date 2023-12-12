b777592fdb080975581ef2fa1d1b9130c167760e
46319943 <495384481@qq.com>
2023-12-12 15:01:58 +0800

    Add cookie pool. Add param for interval.

--- pipeline_manager.py
+++ pipeline_manager.py
@@ -1,12 +1,14 @@
 import time
 
+import request_utils
 from page_extractor import review_page
 from request_utils import get_page
 from pymongo import MongoClient
 
 from argparse import ArgumentParser
 
-def scrape_review(dianping_id, dianping_name, start_page=1):
+
+def scrape_review(dianping_id, dianping_name, start_page, interval_seconds):
     page = start_page
     collection = db[dianping_name]
 
@@ -27,7 +29,8 @@ def scrape_review(dianping_id, dianping_name, start_page=1):
         page += 1
 
         # wait for some seconds
-        time.sleep(5)
+        time.sleep(interval_seconds)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -35,8 +38,13 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--name', help='Dianping Name', required=True)
     parser.add_argument('-p', '--page', help='Start Page', default=1, type=int)
     parser.add_argument('-d', '--database', help='Database Name', default='大众点评')
+    parser.add_argument('-s', '--interval', help='Interval Seconds', default=3, type=int)
+    parser.add_argument('-c', '--pool', help='Cookie Pool Size', default=3, type=int)
+
     args = parser.parse_args()
+
+    request_utils.pool_size = args.pool
 
     client = MongoClient()
     db = client[args.database]
-    scrape_review(args.id, args.name, args.page)
+    scrape_review(args.id, args.name, args.page, args.interval)
--- request_utils.py
+++ request_utils.py
@@ -1,11 +1,30 @@
 from requests import get
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client['大众点评']
+collection = db['CookiePool']
+
+pool_size = 1
+
+def add_cookie():
+    new_cookie = get('http://localhost:3001').text
+    collection.insert_one({'cookie': new_cookie})
 
 
 def get_cookie():
-    return get('http://localhost:3001/').text
+    while collection.count_documents({}) < pool_size:
+        add_cookie()
+
+    # Randomly get a cookie from the pool.
+    cookie = collection.aggregate([{'$sample': {'size': 1}}]).next()['cookie']
+    return cookie
 
 
-cookie_text = get_cookie()
+def delete_cookie(cookie):
+    collection.delete_one({'cookie': cookie})
+
+
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0'
 
 proxies = {
@@ -15,13 +34,17 @@ proxies = {
 
 
 def get_page(url):
-    global cookie_text
+    cookie = get_cookie()
     res = get(url,
               proxies=proxies,
-              headers={'Cookie': cookie_text, 'Referer': 'http://www.dianping.com/', 'User-Agent': user_agent})
+              headers={'Cookie': cookie, 'Referer': 'http://www.dianping.com/', 'User-Agent': user_agent})
     if res.status_code == 403:
         # Re-login is required to refresh the cookie.
-        cookie_text = get_cookie()
+        delete_cookie(cookie)
+        return get_page(url)
+    if '<span class="sub-logo">登录</span>' in res.text:
+        # Re-login is required to refresh the cookie.
+        delete_cookie(cookie)
         return get_page(url)
 
     text = res.text
